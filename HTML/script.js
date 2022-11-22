589604bf74c6e45d52d7196c246908a6c3e3cd0a
PrestonForbes1 <pforbes1227@gmail.com>
2022-11-22 12:58:55 -0700

    first commit

---
+++ HTML/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+
+    $('.menu-button').click(function(){
+        $('navbar.menu').toggleClass("active");
+    });
+    });
---
+++ JavaScript/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+
+$('.menu-button').click(function(){
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-button i').toggleClass("active");
+});
+});
